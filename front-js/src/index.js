e47b99be8986ee08693554510622e59a3335be5e
WarrenB59 <bwarren90@hotmail.fr>
2022-09-19 13:51:08 +0200

    index.js modified

--- front-js/src/index.js
+++ front-js/src/index.js
@@ -4,7 +4,7 @@ fetch("https://nodejs-pokemon-api.herokuapp.com/")
   .then((res) => console.log(res));
 
 // Step 2 : "Get JWT token 🔓"
-fetch("https://nodejs-pokemon-api.herokuapp.com/api/login", {
+fetch("https://cryptic-sands-41262.herokuapp.com/api/login", {
   method: "POST",
   body: JSON.stringify({ username: "pikachu", password: "pikachu" }),
   headers: { "Content-type": "application/json" },
@@ -18,7 +18,7 @@ fetch("https://nodejs-pokemon-api.herokuapp.com/api/login", {
 
 // Step 3 : "Get pokemon list 🎉"
 const fetchPokemonlist = (token) => {
-  fetch("https://nodejs-pokemon-api.herokuapp.com/api/pokemons", {
+  fetch("https://cryptic-sands-41262.herokuapp.com/api/pokemons", {
     headers: { Authorization: `Bearer ${token}` },
   })
     .then((res) => res.json())
